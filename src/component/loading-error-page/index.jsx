b0069d70cba4604f4137cc2092fa558ccd46da1e
杨介正 <hzyangjiezheng@corp.netease.com>
2020-04-08 17:09:54 +0800

    feat: update loading error page

--- src/component/loading-error-page/index.jsx
+++ src/component/loading-error-page/index.jsx
@@ -1,21 +1,38 @@
 import React from 'react';
 
 class LoadingErrorPage extends React.PureComponent {
-    componentDidMount() {
+    state = {}
 
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            this.setState({
+                timeout: true
+            });
+        }, 5000);
+    }
+
+    componentWillUnmount() {
+        clearTimeout(this.timer);
     }
 
     render() {
+        const {
+            timeout
+        } = this.state;
         return (
             <div className="m-loading_page">
                 <div className="content">
-                    <div className="error">
-                        <i />
-                        <div className="desc">
-                            <section>oops~</section>
-                            <section className="secondary">未加载出来，请退出再次尝试</section>
+                    {timeout
+                        && (
+                        <div className="error">
+                            <i />
+                            <div className="desc">
+                                <section>oops~</section>
+                                <section className="secondary">未加载出来，请退出再次尝试</section>
+                            </div>
                         </div>
-                    </div>
+                        )
+                    }
                 </div>
             </div>
         );
